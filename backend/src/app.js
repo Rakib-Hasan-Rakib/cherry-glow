96272db9535c5dace2e68cd5dec1353c1108b8c8
Rakib-Hasan-Rakib <rakibhasanrakib3006@gmail.com>
2026-01-07 14:48:50 +0900

    cors policy updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,12 @@ import orderRoutes from "./routes/orders.routes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://cherry-glow.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
