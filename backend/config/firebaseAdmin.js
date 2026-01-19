25a31125bba95ff32a4380672326161843832791
Rakib-Hasan-Rakib <rakibhasanrakib3006@gmail.com>
2026-01-20 01:08:45 +0900

    type changes from module to commonjs

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 import admin from "firebase-admin";
 
-console.log("FIREBASE_PRIVATE_KEY:", process.env.FIREBASE_PRIVATE_KEY);
 
 if (!admin.apps.length) {
   admin.initializeApp({
